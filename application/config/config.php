95011a43f45248623077efab7e84833cf6f1e128
kindai <kintomiko@gmail.com>
2016-10-18 22:11:18 +1300

    config base url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://bella41981.applinzi.com/';
+$config['base_url'] = 'http://bella-bella981.44fs.preview.openshiftapps.com/';
 
 /*
 |--------------------------------------------------------------------------

fc09e5872d64f5a3cfa6a9c5fa1e6a9e0f97c26d
kindai <kintomiko@gmail.com>
2016-10-24 22:06:24 +1300

    fix behat problem

--- features/activity.feature
+++ features/activity.feature
@@ -2,10 +2,26 @@
 Feature: Activity
 
   @javascript
-  Scenario: 会员登陆之后能看到活动管理
+  Scenario: 炒鸡管理员登陆之后能看到活动管理
     Given I log in with username: "sadmin" and password: "123123"
     And I am on "/club"
-    When I wait for "1" secs
-    And I follow "活动管理"
+    When I keep following "活动管理"
+    Then I wait to see "活动审批"
+    And I wait to see "全部活动"
+    And I wait to see "我的活动"
+
+  @javascript
+  Scenario: 管理员登陆之后能看到活动管理
+    Given I log in with username: "admin" and password: "123123"
+    And I am on "/club"
+    When I keep following "活动管理"
+    Then I wait to see "全部活动"
+    And I wait to see "我的活动"
+
+  @javascript
+  Scenario: 会员登陆之后能看到活动管理
+    Given I log in with username: "user" and password: "123123"
+    And I am on "/club"
+    When I keep following "活动管理"
     Then I wait to see "全部活动"
     And I wait to see "我的活动"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,62 +7,6 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-        date_default_timezone_set("PRC");
-    }
-
-    public function wait($time = 10000, $condition = null)
-    {
-        if (!$this->getSession()->getDriver() instanceof Selenium2Driver) {
-            return;
-        }
-        $start = microtime(true);
-        $end = $start + $time / 1000.0;
-        if ($condition === null) {
-            $defaultCondition = true;
-            $conditions = [
-                "document.readyState == 'complete'",           // Page is ready
-                "typeof $ != 'undefined'",                     // jQuery is loaded
-                "!$.active",                                   // No ajax request is active
-                "$('#page').css('display') == 'block'",        // Page is displayed (no progress bar)
-                "$('.loading-mask').css('display') == 'none'", // Page is not loading (no black mask loading page)
-                "$('.jstree-loading').length == 0",            // Jstree has finished loading
-            ];
-            $condition = implode(' && ', $conditions);
-        } else {
-            $defaultCondition = false;
-        }
-        // Make sure the AJAX calls are fired up before checking the condition
-        $this->getSession()->wait(100, false);
-        $this->getSession()->wait($time, $condition);
-        // Check if we reached the timeout unless the condition is false to explicitly wait the specified time
-        if ($condition !== false && microtime(true) > $end) {
-            if ($defaultCondition) {
-                foreach ($conditions as $condition) {
-                    $result = $this->getSession()->evaluateScript($condition);
-                    if (!$result) {
-                        throw new BehaviorException(
-                            sprintf(
-                                'Timeout of %d reached when checking on "%s"',
-                                $time,
-                                $condition
-                            )
-                        );
-                    }
-                }
-            } else {
-                throw new BehaviorException(sprintf('Timeout of %d reached when checking on %s', $time, $condition));
-            }
-        }
-    }
 
     /**
      * login the club
@@ -79,11 +23,39 @@ class FeatureContext extends MinkContext
     }
 
     /**
+     * @Given /^I wait to see "(?P<text>(?:[^"]|\\")*)"$/
      *
-     * @Given /^(?:|I )wait for "(?P<timespan>[^"]+)" secs$/
+     * @param string $text
+     *
+     * @return bool
      */
-    public function iWait($timespan){
-        $this->getSession()->wait(((int)$timespan)*1000);
+    public function iWaitToSee($text)
+    {
+        $this->waitUntilPageLoaded();
+
+        return $this->spin(
+            function ($context) use ($text) {
+                return parent::assertPageContainsText($text);
+            }
+        );
+    }
+
+    /**
+     * @Given /^I keep following "(?P<page>(?:[^"]|\\")*)"$/
+     *
+     * @param string $text
+     *
+     * @return bool
+     */
+    public function iKeepFollowing($link)
+    {
+        $this->waitUntilPageLoaded();
+
+        return $this->spin(
+            function ($context) use ($link) {
+                return parent::clickLink($link);
+            }
+        );
     }
 
     /**
@@ -111,18 +83,7 @@ class FeatureContext extends MinkContext
     {
         for ($i = 0; $i < $tries; $i++) {
             try {
-                if ($result = $closure($this)) {
-                    if (!is_bool($result)) {
-                        throw new UnsupportedTestException(
-                            'The spinned callback needs to return true on success or throw an Exception'
-                        );
-                    }
-
-                    return true;
-                }
-            } catch (UnsupportedTestException $e) {
-                // If the test is unsupported, we quit
-                throw $e;
+                return $closure($this);
             } catch (\Exception $e) {
                 // do nothing to continue the loop
             }
@@ -134,33 +95,6 @@ class FeatureContext extends MinkContext
         throw new \Exception(
             "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
             "With the following arguments: " . print_r($backtrace[1]['args'], true)
-        );
-    }
-    /**
-     * @Given /^I wait to see "(?P<text>(?:[^"]|\\")*)"$/
-     *
-     * @param string $text
-     *
-     * @return bool
-     */
-    public function iWaitToSee($text)
-    {
-        return $this->assertPageContainsText($text);
-    }
-    /**
-     * Overrides MinkContext method by adding a spin
-     *
-     * {@inheritdoc}
-     */
-    public function assertPageContainsText($text)
-    {
-        $this->waitUntilPageLoaded();
-
-        return $this->spin(
-            function ($context) use ($text) {
-                $context->assertSession()->pageTextContains($context->fixStepArgument($text));
-                return true;
-            }
         );
     }
 
@@ -178,6 +112,18 @@ class FeatureContext extends MinkContext
                 return true;
             }
         );
+    }
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        date_default_timezone_set("PRC");
     }
 
 }
